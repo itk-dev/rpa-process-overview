fcff9a8002869a0478d145ecf24480bdbfb1cf27
Mikkel Ricky <rimi@aarhus.dk>
2025-09-24 19:55:33 +0200

    Cleaned up pagination

--- api/project/app.py
+++ api/project/app.py
@@ -2,9 +2,11 @@
 
 from fastapi import FastAPI, HTTPException
 
-from fastapi import Depends, Request, Query
+from fastapi import Depends, Request, Response
 from sqlmodel import Session, select, SQLModel
 from typing import Generic, TypeVar
+from fastapi_pagination import add_pagination, Page
+from fastapi_pagination.ext.sqlmodel import paginate
 
 from .database import create_db_and_tables, engine
 
@@ -34,6 +36,7 @@ def get_session():
         yield session
 
 app = FastAPI()
+add_pagination(app)
 
 @app.on_event("startup")
 def on_startup():
@@ -41,86 +44,50 @@ def on_startup():
 
 API_PATH_PREFIX = "/api/v1"
 
-@app.get(API_PATH_PREFIX+"/process/", response_model=ProcessListResponse)
-def read_process_list(request: Request, session: Session = Depends(get_session), page: int = 1, page_size: int = Query(default=100, le=100), q: str = None):
+@app.get(API_PATH_PREFIX+"/process/")
+def read_process_list(request: Request, response: Response, session: Session = Depends(get_session), q: str = None) -> Page[ProcessPublic]:
     query = (select(Process)
-             .order_by(Process.id)
-             .offset((page-1)*page_size)
-             # Fetch one more than needed to check if more exist
-             .limit(page_size+1))
+             .order_by(Process.id))
 
     if q is not None:
         query = query.filter(Process.name.like('%{}%'.format(q)))
 
-    items = session.exec(query).all()
+    return _set_pagination_links(request, response, paginate(session, query))
 
-    has_more = False
-    if len(items) > page_size:
-        items = items[:-1]
-        has_more = True
-
-    url = request.url
-    # https://jsonapi.org/format/#fetching-pagination
-    links = {
-        "self": str(url),
-    }
-    if len(items) > 0:
-        links["first"] = str(url.include_query_params(page=1))
-        if page > 1:
-            links["prev"] = str(url.include_query_params(page=page-1))
-        if has_more:
-            links["next"] = str(url.include_query_params(page=page+1))
-
-    return ListResponse(data=items, links=links)
-
-@app.get(API_PATH_PREFIX+"/process/{process_id}", response_model=ProcessResponse)
-def read_process(*, request: Request, session: Session = Depends(get_session), process_id: int):
+@app.get(API_PATH_PREFIX+"/process/{process_id}")
+def read_process(*, request: Request, session: Session = Depends(get_session), process_id: int) -> ProcessPublic:
     process = session.get(Process, process_id)
     if not process:
         raise HTTPException(status_code=404, detail="Process not found")
 
-    url = request.url
-    # https://jsonapi.org/format/#fetching-pagination
-    links = {
-        "self": str(url),
-        "run": str(url)+"/run",
-    }
+    return process
 
-    return ProcessResponse(data=process, links=links)
-
-@app.get(API_PATH_PREFIX+"/process/{process_id}/run", response_model=ProcessRunListResponse)
-def read_process_run(*, request: Request, session: Session = Depends(get_session), process_id: int, page: int = 1, page_size: int = Query(default=10, le=100), q: str = None):
+@app.get(API_PATH_PREFIX+"/process/{process_id}/run")
+def read_process_run(*, request: Request, response: Response, session: Session = Depends(get_session), process_id: int, q: str = None) -> Page[ProcessRunPublic]:
     process = session.get(Process, process_id)
     if not process:
         raise HTTPException(status_code=404, detail="Process not found")
 
     query = (select(ProcessRun)
-             .filter(ProcessRun.process==process)
-             .order_by(ProcessRun.id)
-             .offset((page-1)*page_size)
-             # Fetch one more than needed to check if more exist
-             .limit(page_size+1))
+             .filter(ProcessRun.process==process))
 
     if q is not None:
         query = query.filter(ProcessRun.meta.like('%{}%'.format(q)))
 
-    items = session.exec(query).all()
+    return _set_pagination_links(request, response, paginate(session, query))
 
-    has_more = False
-    if len(items) > page_size:
-        items = items[:-1]
-        has_more = True
+def _set_pagination_links(request: Request, response: Response, pagination: any) -> any:
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Link
+    links = []
+    page = pagination.page
 
-    url = request.url
-    # https://jsonapi.org/format/#fetching-pagination
-    links = {
-        "self": str(url),
-    }
-    if len(items) > 0:
-        links["first"] = str(url.include_query_params(page=1))
-        if page > 1:
-            links["prev"] = str(url.include_query_params(page=page-1))
-        if has_more:
-            links["next"] = str(url.include_query_params(page=page+1))
+    links.append('<{}>; rel="self"'.format(request.url))
+    if page > 1:
+        links.append('<{}>; rel="prev"'.format(request.url.include_query_params(page=page-1)))
+    if page < pagination.pages:
+        links.append('<{}>; rel="next"'.format(request.url.include_query_params(page=page+1)))
 
-    return ProcessRunListResponse(data=items, links=links)
+    if len(links) > 0:
+        response.headers["Link"] = ', '.join(links)
+
+    return pagination
