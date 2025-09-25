d98317b87773103ceb4ae618546c9e7baced290f
Mikkel Ricky <rimi@aarhus.dk>
2025-09-25 10:40:21 +0200

    Run steps

--- api/project/create-data.py
+++ api/project/create-data.py
@@ -1,14 +1,16 @@
 # https://sqlmodel.tiangolo.com/tutorial/fastapi/simple-hero-api/#sqlmodel-code-models-engine
 
 from .database import create_db_and_tables, engine
-from .models import Process, ProcessStep, ProcessRun
+from .models import Process, ProcessStep, ProcessRun, ProcessStepRun
 from datetime import timedelta
-
-import json
 
 from faker import Faker
 
 from sqlmodel import Session
+
+STATUS_PENDING = 'PENDING'
+STATUS_SUCCESS = 'SUCCESS'
+STATUS_FAILED = 'FAILED'
 
 def main():
     create_db_and_tables()
@@ -25,55 +27,62 @@ def main():
             }
             process = Process(
                 name=fake.sentence(4),
-                meta=json.dumps(meta),
+                meta=meta,
             )
+            session.add(process)
+
             number_of_steps = fake.pyint(2, 7)
+            steps = []
             for j in range(number_of_steps):
                 step = ProcessStep(
+                    process = process,
                     index=j,
                     name=fake.sentence(2),
                 )
-                step.process = process
                 session.add(step)
+                steps.append(step)
 
             for j in range(60):
                 meta = {
-                    'cpr': fake.random_number(10, fix_len=True),
+                    'cpr': str(fake.random_number(10, fix_len=True)),
                     'name': fake.name(),
                     'branch': fake.company(),
                 }
-                steps = []
+
+                run = ProcessRun(
+                    process=process,
+                    meta=meta,
+                )
+                session.add(run)
 
                 failed_step_index = fake.pyint(0, number_of_steps+1)
                 started_at = fake.past_datetime()
                 for index in range(0, number_of_steps):
+                    status = STATUS_SUCCESS if index < failed_step_index else STATUS_PENDING
                     started_at = started_at + timedelta(seconds=fake.pyint(1, 1000))
                     finished_at = started_at + timedelta(seconds=fake.pyint(3, 42))
-                    status = 'SUCCESS' if index < failed_step_index else 'PENDING'
                     failure = None
                     if index == failed_step_index:
-                        status = 'FAILED'
+                        status = STATUS_FAILED
+                        occurred_at = (finished_at - timedelta(seconds=-fake.pyint(0, 3))).isoformat()
+                        finished_at = None
                         failure = {
-                            'code': 'Fejlkode',
-                            'message': 'Fejlbesked',
+                            'code': fake.pyint(1, 7),
+                            'message': fake.sentence(),
                             'retryable': fake.boolean(),
-                            'occurred_at': (finished_at - timedelta(seconds=-fake.pyint(0, 3))).isoformat(),
+                            'occurred_at': occurred_at,
                         }
-                    steps.append({
-                        'status': status,
-                        'started_at': started_at.isoformat(),
-                        'finished_at': finished_at.isoformat(),
-                        'failure': failure,
-                    })
+                    step_run = ProcessStepRun(
+                        status=status,
+                        started_at=started_at if status != STATUS_PENDING else None,
+                        finished_at=finished_at if status != STATUS_PENDING else None,
+                        process=process,
+                        run=run,
+                        step=steps[index],
+                        failure=failure,
+                    )
+                    session.add(step_run)
 
-                run = ProcessRun(
-                    meta=json.dumps(meta),
-                    steps=json.dumps(steps),
-                )
-                run.process = process
-                session.add(run)
-
-            session.add(process)
 
         session.commit()
 
--- api/project/models.py
+++ api/project/models.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
+from typing import Any
 
-from sqlmodel import Field, SQLModel, Relationship
+from sqlmodel import Field, SQLModel, Relationship, Column
+from sqlalchemy import JSON
+from datetime import datetime
 
 # Process
 
 class ProcessBase(SQLModel):
     name: str = Field(index=True)
-    meta: str = Field()
+    meta: dict[str, Any] = Field(sa_column=Column(JSON))
 
 class Process(ProcessBase, table=True):
+    __tablename__ = 'process'
+
     id: int | None = Field(default=None, primary_key=True)
 
     steps: list["ProcessStep"] = Relationship(back_populates="process")
@@ -16,7 +21,7 @@ class Process(ProcessBase, table=True):
 
 class ProcessPublic(ProcessBase):
     id: int
-    meta: str
+    meta: dict[str, Any]
     steps: list["ProcessStepPublic"] = []
 
 # Process step
@@ -28,6 +33,8 @@ class ProcessStepBase(SQLModel):
     process_id: int | None = Field(default=None, foreign_key="process.id")
 
 class ProcessStep(ProcessStepBase, table=True):
+    __tablename__ = 'process_step'
+
     id: int | None = Field(default=None, primary_key=True)
 
     process: Process | None = Relationship(back_populates="steps")
@@ -38,16 +45,37 @@ class ProcessStepPublic(ProcessStepBase):
 # Process run
 
 class ProcessRunBase(SQLModel):
-    meta: str = Field()
-    steps: str = Field()
+    meta: dict[str, Any] = Field(sa_column=Column(JSON))
     process_id: int | None = Field(default=None, foreign_key="process.id")
 
 class ProcessRun(ProcessRunBase, table=True):
+    __tablename__ = 'process_run'
+
     id: int | None = Field(default=None, primary_key=True)
 
+    steps: list["ProcessStepRun"] = Relationship(back_populates="run")
     process: Process | None = Relationship(back_populates="runs")
 
 class ProcessRunPublic(ProcessRunBase):
     id: int
-    meta: str
-    steps: str
+    meta: dict[str, Any]
+    steps: list["ProcessStepRun"] = []
+
+# Process step run
+
+class ProcessStepRunBase(SQLModel):
+    status: str
+    started_at: datetime|None
+    finished_at: datetime|None
+    failure: dict[str, Any]|None = Field(sa_column=Column(JSON)) #, schema_extra={'examples': "A very nice Item"})
+
+    run_id: int | None = Field(default=None, foreign_key="process_run.id")
+    step_id: int | None = Field(default=None, foreign_key="process_step.id")
+
+class ProcessStepRun(ProcessStepRunBase, table=True):
+    __tablename__ = 'process_step_run'
+
+    id: int | None = Field(default=None, primary_key=True)
+
+    run: ProcessRun | None = Relationship()
+    step: ProcessStep | None = Relationship()
