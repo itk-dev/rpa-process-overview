bf0cc717d2e8f1bf846caacafcb10fd0544716c4
Mikkel Ricky <rimi@aarhus.dk>
2025-09-29 12:17:10 +0200

    Made fixtures create pending runs

--- api/src/api/fixtures.py
+++ api/src/api/fixtures.py
@@ -69,7 +69,7 @@ class Fixtures:
                     )
                     session.add(run)
 
-                    failed_step_index = fake.pyint(0, number_of_steps + 1)
+                    failed_step_index = fake.pyint(-1, number_of_steps + 1)
                     started_at = fake.past_datetime()
                     for index in range(number_of_steps):
                         status = StepRunStatus.SUCCESS if index < failed_step_index else StepRunStatus.PENDING
