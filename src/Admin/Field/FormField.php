a545e18aa09834c227d730d7b95de62ecbd7f540
Mikkel Ricky <rimi@aarhus.dk>
2025-10-01 13:55:49 +0200

    Cleaned up

--- src/Admin/Field/FormField.php
+++ src/Admin/Field/FormField.php
@@ -2,9 +2,6 @@
 
 namespace App\Admin\Field;
 
-use App\Admin\Form\FormActionType;
-use App\Admin\Form\FormAlertType;
-use App\Admin\Form\FormMessageType;
 use App\Admin\Form\FormTemplateViewType;
 use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Field\FieldTrait;
@@ -14,14 +11,7 @@ final class FormField implements FieldInterface
 {
     use FieldTrait;
 
-    public const OPTION_CONTENT = 'content';
-    public const OPTION_TYPE = 'type';
-    public const OPTION_ACTION = 'action';
     public const OPTION_TEMPLATE_CONTEXT = 'template_context';
-
-    public const TYPE_INFO = 'info';
-    public const TYPE_WARNING = 'warning';
-    public const TYPE_DANGER = 'danger';
 
     /**
      * @internal Use the other named constructors instead (addMessage(), etc.)
@@ -31,50 +21,6 @@ final class FormField implements FieldInterface
     public static function new(string $propertyName, $label = null): never
     {
         throw new \RuntimeException('Instead of this method, use the "addMessage()" method.');
-    }
-
-    public static function addAlert(string|TranslatableInterface $content, string $type = self::TYPE_INFO): self
-    {
-        return new self()
-            ->setFieldFqcn(__CLASS__)
-            ->hideOnIndex()
-            ->setProperty('admin_forms_alert')
-            ->setLabel(false)
-            ->setFormType(FormAlertType::class)
-            ->addCssClass('field-form_alert')
-            ->setFormTypeOptions(['mapped' => false, 'required' => false])
-            ->setValue(true)
-            ->setContent($content)
-            ->setType($type);
-    }
-
-    public static function addAction(string $label, string $action): self
-    {
-        return new self()
-            ->setFieldFqcn(__CLASS__)
-            ->hideOnIndex()
-            ->setProperty('admin_forms_action')
-            ->setLabel(false)
-            ->setFormType(FormActionType::class)
-            ->addCssClass('field-form_action')
-            ->setFormTypeOptions(['mapped' => false, 'required' => false])
-            ->setValue(true)
-            ->setContent($label)
-            ->setAction($action);
-    }
-
-    public static function addMessage(string $content): self
-    {
-        return new self()
-            ->setFieldFqcn(__CLASS__)
-            ->hideOnIndex()
-            ->setProperty('admin_forms_message')
-            ->setLabel(false)
-            ->setFormType(FormMessageType::class)
-            ->addCssClass('field-form_message')
-            ->setFormTypeOptions(['mapped' => false, 'required' => false])
-            ->setValue(true)
-            ->setContent($content);
     }
 
     public static function addTemplateView(string $templatePath, array $context = []): self
@@ -90,27 +36,6 @@ final class FormField implements FieldInterface
             ->setValue(true)
             ->setTemplatePath($templatePath)
             ->setTemplateContext($context);
-    }
-
-    public function setAction(string $action): self
-    {
-        $this->setCustomOption(self::OPTION_ACTION, $action);
-
-        return $this;
-    }
-
-    public function setContent(string $content): self
-    {
-        $this->setCustomOption(self::OPTION_CONTENT, $content);
-
-        return $this;
-    }
-
-    public function setType(string $type): self
-    {
-        $this->setCustomOption(self::OPTION_TYPE, $type);
-
-        return $this;
     }
 
     public function setTemplateContext(array $context): self
