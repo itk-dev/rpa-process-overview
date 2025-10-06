bf8059d7acd9b52ae00f870a9bd9cbf263db1dc1
sinejespersen <sinejespersen@protonmail.com>
2025-10-06 13:05:52 +0200

    5674: add tailwind classes to search

--- widgets/src/_standalone/ProcessSearch/config.ts
+++ widgets/src/_standalone/ProcessSearch/config.ts
@@ -1,7 +1,25 @@
 import ProcessSearch from './index.svelte';
 
 import type { TargetEmbeddedWindow } from 'svelte-standalone';
+import type { SearchConfig } from '$lib/types';
 
 declare global {
 	interface Window extends TargetEmbeddedWindow<typeof ProcessSearch, 'ProcessSearch'> {}
 }
+
+export const config: SearchConfig = (() => {
+	const el = document.querySelector('[data-rpa-process-search-config]') as HTMLElement | null;
+	if (el !== null) {
+		try {
+			const configString = el.dataset.rpaProcessSearchConfig;
+			if (configString) {
+				return JSON.parse(configString);
+			}
+		} catch (error) {
+			// Ignore all errors.
+		}
+	}
+	return {};
+})();
+
+export const t = (text: string) => config.messages?.[text] ?? text + ' (missing translation)';
