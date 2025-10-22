bcca832ebbbca806b29ba5412b94b580bd2a341f
sinejespersen <sinejespersen@protonmail.com>
2025-10-22 10:48:17 +0200

    5708: refactor pagination button into file, and add disabled state to button

--- widgets/src/_standalone/ProcessOverview/Pagination.svelte
+++ widgets/src/_standalone/ProcessOverview/Pagination.svelte
@@ -2,6 +2,7 @@
 	import LeftArrow from '../../lib/Icons/LeftArrow.svelte';
 	import RightArrow from '../../lib/Icons/RightArrow.svelte';
 	import { t } from './config';
+	import PaginationButton from './PaginationButton.svelte';
 
 	let {
 		totalAmount,
@@ -39,36 +40,32 @@
 		<span>{size * page - size}-{size * page} {t('of')} {totalAmount}</span>
 	</div>
 	<div class="flex items-center space-x-2">
-		<button
-			disabled={!isThisTheFirstPage()}
-			aria-describedby="prev-page-label"
-			onclick={() => changePage(page - 1)}
-			class="cursor-pointer flex justify-center items-center h-8 w-8 rounded-md text-gray-800 dark:text-gray-300 dark:hover:bg-violet-600 hover:bg-violet-300 text-gray-100"
+		<PaginationButton
+			disabled={isThisTheFirstPage()}
+			label={t('Go to previous page')}
+			id={'prev'}
+			changePage={() => changePage(page - 1)}
 		>
 			<LeftArrow />
-			<span class="sr-only" id="prev-page-label">{t('Go to previous page')}</span>
-		</button>
+		</PaginationButton>
 		<div class="flex items-center space-x-1">
 			{#each totalAmountOfPagesAsIntegerArray as index}
-				<span class="sr-only" id={`go-to-page-${index}`}>{t('Go to page')}</span>
-				<button
-					aria-describedby={`go-to-page-${index}`}
-					onclick={() => changePage(index)}
-					class="cursor-pointer flex justify-center items-center h-8 w-8 rounded-md text-gray-800 dark:text-gray-300 dark:hover:bg-violet-600 hover:bg-violet-300 text-gray-100 {page ===
-					index
-						? 'bg-violet-600 text-white'
-						: ''}">{index}</button
+				<PaginationButton
+					selected={page === index}
+					label={t('Go to page')}
+					id={String(index)}
+					changePage={() => changePage(index)}>{index}</PaginationButton
 				>
 			{/each}
 		</div>
 
-		<button
-			aria-describedby="next-page-label"
+		<PaginationButton
 			disabled={isThisTheLastPage()}
-			onclick={() => changePage(page + 1)}
-			class="cursor-pointer flex justify-center items-center h-8 w-8 rounded-md text-gray-800 dark:text-gray-300 dark:hover:bg-violet-600 hover:bg-violet-300 text-gray-100"
-			><RightArrow />
-			<span class="sr-only" id="next-page-label">{t('Go to next page')}</span>
-		</button>
+			label={t('Go to next page')}
+			id="next"
+			changePage={() => changePage(page + 1)}
+		>
+			<RightArrow />
+		</PaginationButton>
 	</div>
 </div>
