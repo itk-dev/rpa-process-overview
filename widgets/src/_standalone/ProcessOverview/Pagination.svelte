b565e0e2570cedf9b38bd5ac6ca84abc2bb21523
sinejespersen <sinejespersen@protonmail.com>
2025-10-22 09:10:07 +0200

    5708: disable button instead of disappear-button

--- widgets/src/_standalone/ProcessOverview/Pagination.svelte
+++ widgets/src/_standalone/ProcessOverview/Pagination.svelte
@@ -39,16 +39,15 @@
 		<span>{size * page - size}-{size * page} {t('of')} {totalAmount}</span>
 	</div>
 	<div class="flex items-center space-x-2">
-		{#if !isThisTheFirstPage()}
-			<button
-				aria-describedby="prev-page-label"
-				onclick={() => changePage(page - 1)}
-				class="cursor-pointer flex justify-center items-center h-8 w-8 rounded-md text-gray-800 dark:text-gray-300 dark:hover:bg-violet-600 hover:bg-violet-300 text-gray-100"
-			>
-				<LeftArrow />
-				<span class="sr-only" id="prev-page-label">{t('Go to previous page')}</span>
-			</button>
-		{/if}
+		<button
+			disabled={!isThisTheFirstPage()}
+			aria-describedby="prev-page-label"
+			onclick={() => changePage(page - 1)}
+			class="cursor-pointer flex justify-center items-center h-8 w-8 rounded-md text-gray-800 dark:text-gray-300 dark:hover:bg-violet-600 hover:bg-violet-300 text-gray-100"
+		>
+			<LeftArrow />
+			<span class="sr-only" id="prev-page-label">{t('Go to previous page')}</span>
+		</button>
 		<div class="flex items-center space-x-1">
 			{#each totalAmountOfPagesAsIntegerArray as index}
 				<span class="sr-only" id={`go-to-page-${index}`}>{t('Go to page')}</span>
@@ -62,19 +61,14 @@
 				>
 			{/each}
 		</div>
-		{#if size + page >= totalAmount}
-			<!-- The below is to avoid the UI jumping around because the pagination button disappears -->
-			<div class="h-8 w-8 bg-transparent"></div>
-		{/if}
 
-		{#if !isThisTheLastPage()}
-			<button
-				aria-describedby="next-page-label"
-				onclick={() => changePage(page + 1)}
-				class="cursor-pointer flex justify-center items-center h-8 w-8 rounded-md text-gray-800 dark:text-gray-300 dark:hover:bg-violet-600 hover:bg-violet-300 text-gray-100"
-				><RightArrow />
-				<span class="sr-only" id="next-page-label">{t('Go to next page')}</span>
-			</button>
-		{/if}
+		<button
+			aria-describedby="next-page-label"
+			disabled={isThisTheLastPage()}
+			onclick={() => changePage(page + 1)}
+			class="cursor-pointer flex justify-center items-center h-8 w-8 rounded-md text-gray-800 dark:text-gray-300 dark:hover:bg-violet-600 hover:bg-violet-300 text-gray-100"
+			><RightArrow />
+			<span class="sr-only" id="next-page-label">{t('Go to next page')}</span>
+		</button>
 	</div>
 </div>
